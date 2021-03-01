6e00356125146e874c415b2f5c79aa01ea36070c
Ian Clawson <ianclawson@Ians-MBP-2.guidecx>
2021-03-01 12:54:40 -0700

    adds sequence extension to sort by keypath

---
+++ Sources/PBJCore/Extensions/Sequence+Ext.swift
@@ -0,0 +1,33 @@
+//
+//  Sequence+Ext.swift
+//  
+//
+//  Created by Ian Clawson on 3/1/21.
+//
+
+import Foundation
+
+extension Sequence {
+    func sorted<T: Comparable>(by path: KeyPath<Element, T>) -> [Element] {
+        return self.sorted { (a, b) -> Bool in
+            return a[keyPath: path] < b [keyPath: path]
+        }
+    }
+    func sorted<T: Comparable>(by path: KeyPath<Element, T?>m
+                                default defaultValue: @autoclosure ()->T) -> [Element] {
+        return self.sorted { (a, b) -> Bool in
+            return a[keyPath: path ?? defaultValue()] < b [keyPath: path ?? defaultValue()]
+        }
+    }
+}
+
+// EXAMPLE:
+
+//struct Person {
+//    var givenName: String
+//    var familyName: String
+//}
+//
+//let people: [Person] = []
+//
+//let sortedPeople = people.sorted(by: \.familyName)
