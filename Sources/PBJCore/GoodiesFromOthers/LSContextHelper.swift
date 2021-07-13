936ab228f3237c84e4e5f7efffd529f3d60435ab
Ian Clawson <ianclawson@ians-mbp.lan>
2021-07-12 23:09:01 -0600

    adds more goodies

--- Sources/PBJCore/GoodiesFromOthers/LSContextHelper.swift
+++ Sources/PBJCore/GoodiesFromOthers/LSContextHelper.swift
@@ -1,27 +1,27 @@
-//  From: Jordan Hipwell via Slack.
-
+////  From: Jordan Hipwell via Slack.
 //
-//  LSContextHelper.swift
-//  
+////
+////  LSContextHelper.swift
+////  
+////
+////  Created by Ian Clawson on 3/29/21.
+////
 //
-//  Created by Ian Clawson on 3/29/21.
+//import Foundation
+//import LocalAuthentication
 //
-
-import Foundation
-import LocalAuthentication
-
-func gitGudLSContext() {
-    let laContext = LAContext()
-    if laContext.canEvaluatePolicy(.deviceOwnerAuthentication, error: nil) {
-        switch laContext.biometryType {
-        case .faceID:
-            break //face id asset
-        case .touchID:
-            break //touch id asset
-        case .none:
-            break //fall back to device passcode asset
-        @unknown default:
-            break //something new? 👀
-        }
-    }
-}
+//func gitGudLSContext() {
+//    let laContext = LAContext()
+//    if laContext.canEvaluatePolicy(.deviceOwnerAuthentication, error: nil) {
+//        switch laContext.biometryType {
+//        case .faceID:
+//            break //face id asset
+//        case .touchID:
+//            break //touch id asset
+//        case .none:
+//            break //fall back to device passcode asset
+//        @unknown default:
+//            break //something new? 👀
+//        }
+//    }
+//}
